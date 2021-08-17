511c7cab7ed3679a824537d0865be8b4cfde8774
ShallowFeather <74778927+ShallowFeather@users.noreply.github.com>
2021-08-18 01:26:28 +0800

    Rebuild owo

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,77 @@
-use yew::{Component, ComponentLink, ShouldRender, Html, html, App};
-use wasm_bindgen::prelude::*;
 use yew::prelude::*;
-use input::TextInput;
 
-mod input;
 mod caesar;
 
-#[derive(Clone, PartialEq, Properties, Default)]
-pub struct Properties {
-    str: String,
+enum Msg {
+    Decrypt,
+    Encrypt,
 }
 
-pub enum Msg {
-    Decrypt(String, u8),
-    Encrypt(String, u8),
-}
-
-pub struct Output {
+struct Model {
+    // `ComponentLink` is like a reference to a component.
+    // It can be used to send messages to the component
     link: ComponentLink<Self>,
-    props: Properties,
+    str: String,
+    num: u8,
+    output: String,
 }
 
-impl Component for Output {
+impl Component for Model {
     type Message = Msg;
     type Properties = ();
-    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
             link,
-            props: Properties {
-                str: "owo".to_string(),
-            },
+            str: "".to_string(),
+            num: 0,
+            output: "".to_string(),
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::Decrypt(S, num) => {
-                let res = caesar::decrypt(S, num);
-                self.change_Str(res);
+            Msg::Decrypt => {
+                self.output = caesar::decrypt(self.str.clone(), self.num);
                 true
             }
-            Msg::Encrypt(S, num) => {
-                let res = caesar::encrypt(S, num);
-                self.change_Str(res);
+            Msg::Encrypt => {
+                self.output = caesar::encrypt(self.str.clone(), self.num);
                 true
             }
         }
     }
 
-    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
         false
     }
 
     fn view(&self) -> Html {
+
         html! {
             <div>
-                <TextInput value="New post" onsubmit={self.link.callback(Msg::CreatePost)} />
-                <button class="button" onclick={self.link.callback(|_| Msg::Decrypt)}>
-                        { "decrypt" }
-                </button>
-                <button class="button" onclick={self.link.callback(|_| Msg::Encrypt)}>
-                        { "encrypt" }
-                </button>
+                <input
+                    type="text"
+                    oninput={self.link.callback(|e: InputEvent| {
+                    let input: HtmlTextAreaElement = e.target_unchecked_into();
+                    self.str={self.}
+            })}
+                    {self.str}
+                />
+                <input
+                    type="number" min="0" max="26"
+                    {self.num}/>
+                <button onclick=self.link.callback(|_| Msg::Decrypt)>{ "Decrypt" }</button>
+                <button onclick=self.link.callback(|_| Msg::Encrypt)>{ "Encrypt" }</button>
+                <p>{ self.output } </p>
             </div>
         }
     }
 }
 
-
-
 fn main() {
+    yew::start_app::<Model>();
 }
